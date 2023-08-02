a875f882c811ffc3f8b8c4bbe6739275880f4cfb
Viet Tai <viettaixca123@gmail.com>
2023-08-02 18:59:25 +0700

    Cau hinh sequelize-cli

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(` app listening on port ${port}`)
