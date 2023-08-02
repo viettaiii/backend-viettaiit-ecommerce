6b4ad4d8e2cbe0f5b5168db6bb324b619fb1322d
Viet Tai <viettaixca123@gmail.com>
2023-08-02 22:40:01 +0700

    Set up kết nối đến database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const connectDB = require("./config/connectDB");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const startServer = async (app, port) => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-app.listen(port, () => {
-  console.log(` app listening on port ${port}`)
-})
+startServer(app, port);
