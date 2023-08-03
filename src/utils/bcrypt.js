b109be87295fd35ce9411e62fb6d9e033fc25f7a
Viet Tai <viettaixca123@gmail.com>
2023-08-03 22:32:16 +0700

    Update các hàm mã hóa password

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,12 +1,17 @@
 const bcrypt = require("bcryptjs");
 
-exports.hashPassword = async (password) => {
+const hashPassword = async (password) => {
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
   return hash;
 };
 
-exports.comparePassword = async (password , hashPassword) => {
+const comparePassword = async (password, hashPassword) => {
   const isMatch = await bcrypt.compareSync(password, hashPassword);
   return isMatch;
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
 };
