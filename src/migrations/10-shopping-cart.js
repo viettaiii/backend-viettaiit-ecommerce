1dc6e2c2ef2a8e12055255e4e92e29f9d2b45ce6
Viet Tai <viettaixca123@gmail.com>
2023-08-03 09:58:29 +0700

    Tạo table shopping-cart và tạo quan hệ với User n-1

---
+++ src/migrations/10-shopping-cart.js
@@ -0,0 +1,33 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("ShoppingCarts", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUID,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("ShoppingCarts");
+  },
+};
