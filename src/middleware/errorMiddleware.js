7676e547c2c657e97ec98d05a979d49415cf523b
Viet Tai <viettaixca123@gmail.com>
2023-08-03 12:45:51 +0700

    Handler error

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,30 @@
+const { StatusCodes } = require("http-status-codes");
+
+const errorMiddleware = (err, req, res, next) => {
+  const defaultError = {
+    status: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    error: {
+      message: err.message || "Internal Server Error",
+      statusText: null,
+      status: err.statusCode,
+      ok: false,
+    },
+  };
+  switch (err.statusCode) {
+    // Bad request
+    case 400:
+      defaultError.error.statusText = "Bad Request";
+      break;
+    case 401:
+      defaultError.error.statusText = "Unauthorized";
+      break;
+    case 404:
+      defaultError.error.statusText = "Not Found";
+      break;
+  }
+  res.status(defaultError.status).json({
+    error: defaultError.error,
+  });
+};
+
+module.exports = errorMiddleware;
