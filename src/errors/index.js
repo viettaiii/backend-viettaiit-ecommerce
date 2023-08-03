2361a6ca668bba0508125729a4b0b24b2224dbcd
Viet Tai <viettaixca123@gmail.com>
2023-08-03 11:49:12 +0700

    Custom error

---
+++ src/errors/index.js
@@ -0,0 +1,8 @@
+const NotFoundError = require("./notFoundError");
+const BadRequestError = require("./badRequestError");
+const UnauthorizedError = require("./unauthorizedError");
+module.exports = {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+};
