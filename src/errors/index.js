dcaf5a3c7aa57d904b919edd77341dd71d0cb5be
Viet Tai <viettaixca123@gmail.com>
2023-08-03 22:24:57 +0700

    Custom error

--- src/errors/index.js
+++ src/errors/index.js
@@ -3,10 +3,11 @@ const BadRequestError = require("./badRequestError");
 const UnauthorizedError = require("./unauthorizedError");
 const ConflictError = require("./conflictError");
 const UnprocessableEntityError = require("./unprocessableEntityError");
+const ForBiddenError = require("./forBiddenError");
 module.exports = {
   NotFoundError,
   BadRequestError,
   UnauthorizedError,
   ConflictError,
-  UnprocessableEntityError,
+  UnprocessableEntityError,ForBiddenError
 };
