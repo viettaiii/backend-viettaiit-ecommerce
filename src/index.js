a5bacb8ae3913ca39840b774406f3a3eb84c8ae5
Viet Tai <viettaixca123@gmail.com>
2023-08-03 15:48:22 +0700

    Cấu hình router cho auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
+// Xử lí bất đồng bộ lỗi
+require("express-async-errors");
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
 const connectDB = require("./config/connectDB");
+const routerAuth = require("./routes/auth");
 
+const { notFoundMiddleware, errorMiddleware } = require("./middleware");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+// Kết tối đến các tuyển đường
+app.use("/api/v1/auth", routerAuth);
 
+// Error middleware
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+// Kết nối đến database và server
+const port = process.env.PORT || 8080;
 const startServer = async (app, port) => {
   try {
     await connectDB();
@@ -15,5 +28,4 @@ const startServer = async (app, port) => {
     console.log(error);
   }
 };
-
 startServer(app, port);
