a42d173546e9cebec96b2ebe0c5cafd4568f3914
Viet Tai <viettaixca123@gmail.com>
2023-08-04 21:20:46 +0700

    Update error middleware response

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -3,44 +3,41 @@ const { StatusCodes } = require("http-status-codes");
 const errorMiddleware = (err, req, res, next) => {
   const defaultError = {
     status: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    error: {
-      message: err.message || "Internal Server Error",
-      statusText: null,
-      status: err.statusCode,
-      ok: false,
-    },
+    message: err.message || "Internal Server Error",
+    statusText: null,
+    status: err.statusCode,
+    ok: false,
   };
   switch (err.statusCode) {
     // Bad request
     case 400:
-      defaultError.error.statusText = "Bad Request";
+      defaultError.statusText = "Bad Request";
       break;
     case 401:
-      defaultError.error.statusText = "Unauthorized";
+      defaultError.statusText = "Unauthorized";
       break;
     case 404:
-      defaultError.error.statusText = "Not Found";
+      defaultError.statusText = "Not Found";
       break;
 
     case 409:
-      defaultError.error.statusText = "Conflict";
+      defaultError.statusText = "Conflict";
       break;
     case 422:
-      defaultError.error.statusText = "Unprocessable Entity";
+      defaultError.statusText = "Unprocessable Entity";
       break;
   }
   if (err.name === "SequelizeValidationError") {
-    defaultError.error.message =
+    defaultError.message =
       err.errors[0].path +
       " có giá trị " +
       err.errors[0].value +
       " không hợp lệ";
-    defaultError.error.statusText = "Unprocessable Entity";
-    defaultError.error.status = StatusCodes.UNPROCESSABLE_ENTITY;
+    defaultError.statusText = "Unprocessable Entity";
     defaultError.status = StatusCodes.UNPROCESSABLE_ENTITY;
   }
   res.status(defaultError.status).json({
-    error: defaultError.error,
+    message: defaultError.message,
   });
 };
 
