1731d49d0964670854a4694c6b1f608fa0413696
Viet Tai <viettaixca123@gmail.com>
2023-08-04 21:19:54 +0700

    sử dụng middleware cors để cho phép origin client truy cập vào server

--- src/index.js
+++ src/index.js
@@ -8,14 +8,22 @@ const routerAuth = require("./routes/auth");
 
 const { notFoundMiddleware, errorMiddleware } = require("./middleware");
 const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
+const cors = require("cors");
 const configViewEngine = require("./config/viewEngine");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
-
-
-app.use(cookieParser())
+app.use(cookieParser());
 // Kết nối view engine
 configViewEngine(app);
+app.get("/auth/verify-email", (req, res) => {
+  res.render("verifyEmail.ejs");
+});
 // Kết tối đến các tuyển đường
 app.use("/api/v1/auth", routerAuth);
 
