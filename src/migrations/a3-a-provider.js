198f8a4a2701f62a2f8d309cc394e6757b791365
Viet Tai <viettaixca123@gmail.com>
2023-08-10 19:35:10 +0700

    Thêm table provider

---
+++ src/migrations/a3-a-provider.js
@@ -0,0 +1,29 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Providers", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUID,
+      },
+      providerName: {
+        type: Sequelize.ENUM,
+        values: ["Apple", "Samsung", "Oppo", "Xiaomi"],
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Providers");
+  },
+};
---
+++ src/models/provider.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Provider extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Product
+      this.hasMany(models.Product, {
+        foreignKey: "providerId",
+        as: "provider",
+      });
+    }
+  }
+  Provider.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      providerName: {
+        type: DataTypes.ENUM,
+        values: ["Apple", "Samsung", "Oppo", "Xiaomi"],
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Provider",
+      timestamps: true,
+    }
+  );
+  return Provider;
+};
