8eaf742fa5b58a591e2ee365aa579594fdc69d40
Viet Tai <viettaixca123@gmail.com>
2023-08-15 15:49:30 +0700

    Update file view ejs

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const path = require("path");
 const configViewEngine = (app) => {
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use("/static", express.static("public"));
   app.set("view engine", "ejs");
   app.set("views", "./src/views");
 };
